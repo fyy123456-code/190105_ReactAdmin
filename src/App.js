63a2b4d51716eb34fb646f09a961e35bf615c1fe
fyy <302656804@qq.com>
2021-09-24 20:38:04 +0800

    redux完成计数案例

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {createIncrementAction, createDecrementAction} from './redux/action_creators'
 
 export default class App extends Component {
+  increment = ()=>{
+    let {value} = this.refs.selectNumber;
+    this.props.store.dispatch(createIncrementAction(value*1))
+  }
+
+  decrement = ()=>{
+    let {value} = this.refs.selectNumber;
+    this.props.store.dispatch(createDecrementAction(value*1))
+  }
+
+  incrementIfOdd=()=>{
+    let {value} = this.refs.selectNumber;
+    if(this.props.store.getState()%2 === 1){
+      this.props.store.dispatch(createIncrementAction(value*1));
+    }
+  }
+
+  incrementAsync = ()=>{
+    let {value} = this.refs.selectNumber;
+    setTimeout(()=>{
+      this.props.store.dispatch(createIncrementAction(value*1))
+    },1000)
+    
+  }
   render() {
+    let count = this.props.store.getState()
     return (
       <div>
-        App
+        <h3>当前求和为：{count}</h3>
+        <select ref='selectNumber'>
+          <option value='1'>1</option>
+          <option value='2'>2</option>
+          <option value='3'>3</option>
+        </select>&nbsp;
+        <button onClick={this.increment}>+</button>&nbsp;
+        <button onClick={this.decrement}>-</button>&nbsp;
+        <button onClick={this.incrementIfOdd}>incrementIfOdd</button>&nbsp;
+        <button onClick={this.incrementAsync}>incrementAsync</button>
       </div>
     )
   }
