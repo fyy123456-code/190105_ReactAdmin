21c439b15cc1f2ee8861a7cb544f56885557500a
fyy <302656804@qq.com>
2021-09-25 10:05:22 +0800

    异步增加

--- src/redux/action_creators.js
+++ src/redux/action_creators.js
@@ -1,8 +1,12 @@
 import {INCREMENT, DECREMENT} from './action_types'
 export const createIncrementAction = value => ({type:INCREMENT, data:value});
 export const createDecrementAction = value => ({type:DECREMENT, data:value});
+
+//创建一个异步action 用于增加
 export const createIncrementAsyncAction = (value, delay) => {
-  setTimeout(()=>{
-    return {type:INCREMENT, data:value}
-  },delay)
+  return (dispatch)=>{
+    setTimeout(()=>{
+      dispatch(createIncrementAction(value))
+    },delay)
+  }
 }
